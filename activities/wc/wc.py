be055f0292f9002929e1cda903e5610f5bbcd404
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2025-02-04 20:03:35 -0800

    fix: Use recommended quotation marks for docstrings

--- activities/wc/wc.py
+++ activities/wc/wc.py
@@ -1,11 +1,11 @@
-'''This program counts the number of lines it contains.
+"""This program counts the number of lines it contains.
 
 It is inspired by the `wc` Unix utility (for details, run the command `man wc`
 on any Unix-like operating systems).
 
 This program differs in at least two ways compared to `wc`.  One difference is
 rather subtle.  Can you find out?
-'''
+"""
 
 def main():
     l = 0
