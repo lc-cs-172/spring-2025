888c0bf2fcafe4eb0418d914613c490086ba3bac
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2025-05-01 17:13:11 -0700

    feat: Complete the implementation of binary search

--- activities/search/binary.py
+++ activities/search/binary.py
@@ -1,7 +1,11 @@
 def _search(haystack, needle, lo : int, hi : int) -> int:
     """Search for needle in sorted haystack[lo:hi), if found return the index
     else -1."""
-    pass
+    if hi <= lo: return -1
+    mid = (lo + hi)//2
+    if haystack[mid] < needle: return _search(haystack, needle, mid + 1, hi)
+    if haystack[mid] > needle: return _search(haystack, needle, lo, mid)
+    return mid
 
 def search(haystack, needle) -> int:
     """Search for needle in sorted haystack, if found return the index else
